64e8117fc74211cdec8714c3ad6805c0adee0eed
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-05-10 21:22:46 +0430

    add navbar and header

---
+++ controller.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {
+    const currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos > 80) {
+        $("nav").animate({opacity: '100%'},"slow");
+        $("nav").css("background-color", "#fff");
+        $("#nav-text").css("opacity", "100%");
+        $("#nav-text").css("color", "black");
+    } else {
+        $("nav").css("background-color", "transparent");
+        $("nav").css("opacity", "50%");
+        $("#nav-text").css("opacity", "30%");
+        $("#nav-text").css("color", "#fff");
+    }
+  }
